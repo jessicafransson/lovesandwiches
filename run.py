17852445649d027aeeb38313029f793383f7a289
Jessica Fransson <jessicafransson@outlook.com>
2022-08-15 09:42:52 +0000

    added code for internal program funciton

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import json
 
 
 SCOPE = [
@@ -8,6 +9,8 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
     
+creds = json.load(open('creds.json'))
+CREDS = Credentials.from_service_account_info(creds)
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
